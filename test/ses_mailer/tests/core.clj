6d3f149a6a91830d445150e298e40ab630f717e6
jstaffans <johannes.staffans@gmail.com>
2015-07-05 22:44:39 +0200

    Tests

--- test/ses_mailer/tests/core.clj
+++ test/ses_mailer/tests/core.clj
@@ -10,4 +10,20 @@
   ["a" "b"]
   (.getToAddresses (ses/create-destination ["a" "b"])))
 
+(expect
+  ""
+  (-> (ses/create-body nil nil)
+    (.getText)
+    (.getData)))
 
+(expect
+  "<html></html>"
+  (-> (ses/create-body "<html></html>" nil)
+    (.getHtml)
+    (.getData)))
+
+(expect
+  "Text"
+  (-> (ses/create-body "<html></html>" "Text")
+    (.getText)
+    (.getData)))
